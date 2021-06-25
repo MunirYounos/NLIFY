e1a79ec858ef5413b9957cc7174c67ad6124cfa8
Munir <munir@anodyne.dk>
2021-06-25 12:28:35 +0200

    Delivery order counter is done on product page ...

--- assets/nlify-counter.js
+++ assets/nlify-counter.js
@@ -1,61 +1,108 @@
-const counterNew = document.getElementById('counterNew');
-const settings = {
-	'Title': 'Spring sale - save 25% on all - sale ends in: ',
-	'End_time': 'July 29, 2021 10:23:59',
-	'Days': 'Days',
-	'Hours': 'Hours',
-	'Minutes': 'Minutes',
-	'Seconds': 'Seconds',
-	'Link': 'https://anodyne.dk/'
-};
-//add zero
-function addZero(n) {
-	return (parseInt(n, 10) < 10 ? '0' : '') + n;
+//topbar counter
+let domTopId = document.getElementById('nlify___js-counter');
+if (domTopId !== null) {
+	weekDayNames(domTopId);
 }
-
-let template = document.createElement('section');
-template.classList = 'new-counter wrapper';
-template.innerHTML = `<h4 id="head">${settings['Title']}</h4>
-        <ul class="counter__wrapper">
-        <li><span id="days"></span>${settings['Days']}</li>
-        <li><span id="hours"></span>${settings['Hours']}</li>
-        <li><span id="minutes"></span>${settings['Minutes']}</li>
-        <li><span id="seconds"></span>${settings['Seconds']}</li>
-        </ul>
-				<a class="new-counter-btn" href="${settings['Link']}"></a>
-				`;
-const second = 1000,
-	minute = second * 60,
-	hour = minute * 60,
-	day = hour * 24;
-
-if (isValidDate(new Date(settings['End_time']))) {
-	let countDown = new Date(settings['End_time']).getTime();
-	const x = setInterval(function () {
-		let now = new Date().getTime(),
-			distance = countDown - now;
-
-
-		if (distance > 0) {
-			counterNew.prepend(template);
-		}
-
-		document.querySelector('.new-counter #days').innerText = Math.floor(distance / (day)),
-			document.querySelector('.new-counter #hours').innerText = addZero(Math.floor((distance % (day)) / (hour))),
-			document.querySelector('.new-counter #minutes').innerText = addZero(Math.floor((distance % (hour)) / (minute))),
-			document.querySelector('.new-counter #seconds').innerText = addZero(Math.floor((distance % (minute)) / second));
-
-		// Hide when date is reached
-		if (distance < 0) {
-			clearInterval(x);
-			document.querySelector('.new-counter.wrapper').style.display = 'none';
-		}
-
-	}, second);
-} else {
-	console.error('End date is invalid');
+function weekDayNames(Id1) {
+	let d = new Date();
+	weekday = d.getDay(),
+		dayslabel = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		weekday = dayslabel[d.getDay()];
+	//switcher
+	switch (weekday) {
+		case 'Fri':
+			calculateDeliveryTimeFriday(Id1, d, 14);
+			break;
+		case 'Sat':
+			calculateDeliveryTimeSaturday(Id1)
+			break;
+		case 'Sun':
+			calculateDeliveryTimeEveryDay(Id1, d, 0);
+			break;
+		default:
+			calculateDeliveryTimeEveryDay(Id1, d, 14);
+	}
 }
-
-function isValidDate(d) {
-	return d instanceof Date && !isNaN(d);
+function calculateDeliveryTimeEveryDay(someId, currentDate, setTime) {
+	let putDateOfNow = currentDate,
+		startDateOfToday = putDateOfNow.setHours(setTime, 0, 0),
+		endDateOfToday = putDateOfNow.setHours(23, 59, 59);
+	//add zeros
+	function addZero(num) {
+		return ("0" + parseInt(num)).substr(-2);
+	}
+	function tick() {
+		let now = new Date();
+		if (now <= startDateOfToday) {
+			let remain = ((startDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+			someId.innerHTML = `<div class="timer__wrapper">
+														<div class="timer__text">
+																Get your order shipped today - order within: 
+														</div>
+														<div class="timer__counter">  
+														<span class="time-go">
+														<span class="time-hour"> <strong>${hours} </strong><span class="time__badge">hours</span></span> <code>:</code> 
+														<span class="time-min"> <strong>${min}</strong><span class="time__badge">minuttes</span></span> <code>:</code> 
+														<span class="time-sec"> <strong>${sec}</strong><span class="time__badge"> seconds</span></span> 
+												</span></div></div>`;
+			setTimeout(tick, 1000);
+		} else if (now < endDateOfToday) {
+			if (now >= endDateOfToday) { // too late, go to tomorrow
+				after.setDate(after.getDate() + 1);
+			}
+			let remain = ((endDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+			someId.innerHTML = `<div class="timer__wrapper">
+													<div class="timer__text">
+														We are sending your parcel tomorrow - order within: 
+													</div>
+													<div class="timer__counter">
+													<span class="time-up"  >
+														<span class="time-hour"> <strong>${hours} </strong><span class="time__badge">hours</span></span> <code>:</code>  
+														<span class="time-min"> <strong>${min}</strong><span class="time__badge">minuttes</span></span> <code>:</code> 
+														<span class="time-sec"> <strong>${sec}</strong><span class="time__badge"> seconds</span></span> 
+													</span></div></div>`;
+			setTimeout(tick, 1000);
+		}
+	}
+	tick();
+}
+function calculateDeliveryTimeFriday(someId, currentDate, setTime) {
+	let putDateOfNow = currentDate,
+		startDateOfToday = putDateOfNow.setHours(setTime, 0, 0);
+	//add zeros
+	function addZero(num) {
+		return ("0" + parseInt(num)).substr(-2);
+	}
+	function tick() {
+		let now = new Date();
+		if (now <= startDateOfToday) {
+			let remain = ((startDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+			someId.innerHTML = `<div class="timer__wrapper">
+							<div class="timer__text">
+									Get your order shipped today - order within: 
+							</div>
+							<div class="timer__counter">  
+							<span class="time-go">
+							<span class="time-hour"> <strong>${hours} </strong><span class="time__badge">hours</span></span> <code>:</code> 
+							<span class="time-min"> <strong>${min}</strong><span class="time__badge">minuttes</span></span> <code>:</code> 
+							<span class="time-sec"> <strong>${sec}</strong><span class="time__badge"> seconds</span></span> 
+							</span></div></div>`;
+			setTimeout(tick, 1000);
+		} else if (now > startDateOfToday) {
+			someId.innerHTML = `<div class="timer__wrapper"><div class="timer__text">Free delivery on orders over 59 &euro;. Free return and exchange</div></div>`;
+		}
+	}
+	tick();
+}
+function calculateDeliveryTimeSaturday(someId) {
+	someId.innerHTML = `<div class="timer__wrapper"><div class="timer__text">Free delivery on orders over 59 &euro;. Free return and exchange</div></div>`;
 }
